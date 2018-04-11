55b44020028c145ed9f01e5f05739488a9eccd7c
hadesyi <leesk55@gmail.com>
2018-04-12 00:18:03 +0900

    Go의 switch 문에서 한가지 특징적인 용법은 switch 뒤에 조건변수 혹은 Expression을 적지 않는 용법이다. 이 경우 각 case 조건문들을 순서대로 검사하여 조건에 맞는 경우 해당 case 블럭을 실행하고 switch문을 빠져나온다. 이 용법은 복잡한 if...else if...else if... 문장을 단순화하는데 유용하다.

--- main/Switch.go
+++ main/Switch.go
@@ -19,3 +19,18 @@ func main() {
 
 	}
 }
+
+func grade(score int) {
+	switch {
+	case score >= 90:
+		println("A")
+	case score >= 80:
+		println("B")
+	case score >= 70:
+		println("C")
+	case score >= 60:
+		println("D")
+	default:
+		println("No Hope")
+	}
+}
